3bfb004158dd71dbcc71212bc040675eabcbc127
vfilippov <vfilippov@6955db30-a419-402b-8a0d-67ecbb4d7f56>
2008-12-08 12:32:42 +0000

    asynchronous SVN update script
    git-svn-id: svn://svn.office.custis.ru/3rdparty/viewvc.org/trunk@44 6955db30-a419-402b-8a0d-67ecbb4d7f56

---
+++ bin/svnupdate-async.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+VIEWVC_DIR=$1
+
+test -f "$VIEWVC_DIR/.svn-updating" -o ! -f "$VIEWVC_DIR/.svn-updated" && exit 0
+mv "$VIEWVC_DIR/.svn-updated" "$VIEWVC_DIR/.svn-updating"
+"$VIEWVC_DIR/bin/svnupdate-async" "$VIEWVC_DIR/bin/svndbadmin" "$VIEWVC_DIR/.svn-updating" | sh >/dev/null
+rm "$VIEWVC_DIR/.svn-updating"
