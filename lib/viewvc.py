c78060051982965229303f57927f30517e9de214
cmpilato <cmpilato@8cb11bc2-c004-0410-86c3-e597b4017df7>
2013-04-24 13:27:13 +0000

    Tag the 1.1.20 final release.
    git-svn-id: http://viewvc.tigris.org/svn/viewvc/tags/1.1.20@2898 8cb11bc2-c004-0410-86c3-e597b4017df7

--- lib/viewvc.py
+++ lib/viewvc.py
@@ -14,7 +14,7 @@
 #
 # -----------------------------------------------------------------------
 
-__version__ = '1.1.20-dev'
+__version__ = '1.1.20'
 
 # this comes from our library; measure the startup time
 import debug
